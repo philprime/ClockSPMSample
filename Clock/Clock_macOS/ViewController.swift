f27aee1757f94e5b763e6dc93b19afff8cbde3dc
Philip Niedertscheider <dev@philpri.me>
2021-04-19 12:41:07 +0200

    Added macOS target

---
+++ Clock/Clock_macOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Clock_macOS
+//
+//  Created by Philip Niedertscheider on 19.04.21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
