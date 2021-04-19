4a9622dd9042a5a2b38e233b26870ef68b7d35d5
Philip Niedertscheider <dev@philpri.me>
2021-04-19 13:27:08 +0200

    Added per-platform package

--- ClockPackage/Package.swift
+++ ClockPackage/Package.swift
@@ -6,10 +6,15 @@ let package = Package(
     name: "ClockPackage",
     products: [
         .library(name: "ClockUI", targets: ["ClockUI"]),
+        .library(name: "ClockUI_iOS", targets: ["ClockUI_iOS"]),
+        .library(name: "ClockUI_macOS", targets: ["ClockUI_macOS"]),
     ],
     targets: [
-        .target(name: "ClockUI", resources: [
+        .target(name: "ClockUI"),
+        .target(name: "ClockUI_iOS", resources: [
             .process("Resources/ClockViewController_iOS.xib"),
+        ]),
+        .target(name: "ClockUI_macOS", resources: [
             .process("Resources/ClockViewController_macOS.xib"),
         ])
     ]
