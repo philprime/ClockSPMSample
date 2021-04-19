d991f6f7ecf44b22ede3d77c33c17cfd8b8ef452
Philip Niedertscheider <dev@philpri.me>
2021-04-19 12:51:35 +0200

    Added iOS app

---
+++ Clock/Clock_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clock_iOS
+//
+//  Created by Philip Niedertscheider on 19.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
